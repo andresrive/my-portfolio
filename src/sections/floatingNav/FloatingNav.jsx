86974acf9ce14a0ef9b167f4901595ddc83dd51c
Andres Rivera <andreseriverar@gmail.com>
2023-04-20 21:11:24 +0200

    arreglos en floatingNav

--- src/sections/floatingNav/FloatingNav.jsx
+++ src/sections/floatingNav/FloatingNav.jsx
@@ -2,15 +2,16 @@ import './floatingNav.css'
 import React from 'react'
 import data from './data'
 import Nav from './Nav'
+import Scrollspy from 'react-scrollspy'
 
 const FloatingNav = () => {
     return (
         <ul id='floating__nav'>
-            <div offset={-500} className='scrollspy' items={['header', 'about', 'services', 'portfolio', 'technologies', 'contact']} currentClassName='active'>
+            <Scrollspy offset={-500} className='scrollspy' items={['header', 'about', 'services', 'portfolio', 'technologies', 'contact']} currentClassName='active'>
                 {
                     data.map(item => <Nav key={item.id} item={item} />)
                 }
-            </div>
+            </Scrollspy>
         </ul>
 
 
--- src/sections/floatingNav/data.jsx
+++ src/sections/floatingNav/data.jsx
@@ -9,8 +9,8 @@ import { AiFillMessage } from 'react-icons/ai'
 const data = [
     { id: 1, link: '#', icon: <AiFillHome /> },
     { id: 2, link: '#about', icon: <BsPersonFill /> },
-    { id: 3, link: '#services', icon: <MdHomeRepairService /> },
-    { id: 4, link: '#portfolio', icon: <AiFillAppstore /> },
+    { id: 3, link: '#services', icon: <AiFillAppstore /> },
+    { id: 4, link: '#portfolio', icon: <MdHomeRepairService /> },
     { id: 5, link: '#technologies', icon: <HiOutlineCode /> },
     { id: 6, link: '#contact', icon: <AiFillMessage /> },
 ]
